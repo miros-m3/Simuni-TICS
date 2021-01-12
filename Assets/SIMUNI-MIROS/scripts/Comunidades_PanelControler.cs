b0a08f31dfae8ecbc5531646a1de0b31e3008b1f
miros-m3 <72417314+miros-m3@users.noreply.github.com>
2021-01-11 18:38:28 -0600

    small changes

---
+++ Assets/SIMUNI-MIROS/scripts/Comunidades_PanelControler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Comunidades_PanelControler : MonoBehaviour
+{
+    //COMUNITY PANELS
+    public GameObject Comunidad;
+    public GameObject NuevoPost;
+    public GameObject ComentarioDetalle;
+
+
+    //MENU PANELS
+
+    //NEWPOST INPUTS
+
+    public void BTN_Comunidad()
+    {
+        Comunidad.SetActive(true);
+
+    }
+    public void BTN_NuevoPost()
+    {
+        NuevoPost.SetActive(true);
+
+    }
+    public void BTN_ComentarioDetalle()
+    {
+        ComentarioDetalle.SetActive(true);
+
+    }
+
+}
